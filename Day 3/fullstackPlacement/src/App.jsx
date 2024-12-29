1117f70e46f6bfc8869b38e31c69c1560a04b601
vignesh <vignesh.k1750@gmail.com>
2024-12-29 23:07:04 +0530

    Day-3 task completed

---
+++ Day 3/fullstackPlacement/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import ClassCompEg from './Components/ClassComponents/ClassCompEg';
+import DefaultExplanation from './Components/FunctionalComponents/DefaultExplanation';
+import UnorderedList from './Components/FunctionalComponents/UnorderedList';
+
+function App() {
+ 
+
+  return (
+    <div>
+      <ClassCompEg />
+      <DefaultExplanation />
+      <UnorderedList />
+      </div>
+  )
+}
+
+export default App;
---
+++ Day 3/fullstackPlacement/src/Components/ClassComponents/ClassCompEg.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+class ClassCompEg extends React.Component{
+render()
+{
+    return(
+        <div>
+            <h1>
+                This is class component example
+            </h1>
+        </div>
+    )
+}
+}
+export default ClassCompEg;
